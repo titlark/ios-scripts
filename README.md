5ce3acd2d576103f78d1cdc7af8c2754521e4c26
Lark <ziye_huang@163.com>
2023-04-21 13:53:03 +0800

    add script.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 #### 圈X 
 
-| 名称    | 地址  | 备注  |
-|-------|-----|-----|
-| NeoAI |   https://raw.githubusercontent.com/titlark/ios-scripts/main/qx/NeoAI.js  |     |
+| 名称          | 地址  | 备注  |
+|-------------|-----|-----|
+| NeoAI       |   https://raw.githubusercontent.com/titlark/ios-scripts/main/qx/NeoAI.js  |     |
+| GoodChatBot |   https://raw.githubusercontent.com/titlark/ios-scripts/main/qx/GoodChatbot.js  |     |
+| 抖音极速版       |   https://raw.githubusercontent.com/titlark/ios-scripts/main/qx/DouYinSpeedVersion.js  |     |
 
---
+++ qx/GoodChatbot.js
@@ -0,0 +1,53 @@
+/*
+‼️规则完全免费，仅供学习交流，🈲️商业用途 by 你有胸我有弟
+
+[rewrite_local]
+https:\/\/api\.revenuecat\.com\/v1\/subscribers\/cIrlSOwSOi-n-PD3j6DEh_EY url script-response-body GoodChatbot.js
+
+[mitm] 
+hostname = buy.itunes.apple.com
+
+下载链接：https://apps.apple.com/cn/app/good-chatbot/id1667468949
+
+版本号：1.5.40
+
+技巧：
+   需要注册登录后，点击Restore Purchase，在订阅信息的最上面图片的中间位置，有点模糊。
+*/
+
+var obj = JSON.parse($response.body);
+obj = {
+    "request_date_ms": 1680875165503, "request_date": "2023-04-07T13:46:05Z", "subscriber": {
+        "last_seen": "2023-04-07T13:45:17Z",
+        "first_seen": "2023-04-07T13:33:13Z",
+        "original_application_version": "2",
+        "non_subscriptions": {},
+        "other_purchases": {},
+        "management_url": "itms-apps://apps.apple.com/account/subscriptions",
+        "subscriptions": {
+            "aichat_premium_yearly1": {
+                "is_sandbox": false,
+                "period_type": "trial",
+                "billing_issues_detected_at": null,
+                "unsubscribe_detected_at": null,
+                "expires_date": "2999-12-31T23:59:59Z",
+                "grace_period_expires_date": null,
+                "original_purchase_date": "2023-04-05T13:10:37Z",
+                "purchase_date": "2023-04-05T13:10:37Z",
+                "store": "app_store"
+            }
+        },
+        "entitlements": {
+            "premium": {
+                "expires_date": "2999-12-31T23:59:59Z",
+                "purchase_date": "2023-04-05T13:10:37Z",
+                "product_identifier": "aichat_premium_yearly1",
+                "grace_period_expires_date": null
+            }
+        },
+        "original_purchase_date": "2023-04-05T13:10:37Z",
+        "original_app_user_id": "$RCAnonymousID:2cbe832a3f3e4ce28fbec3266e2a5fca",
+        "entitlement": {}
+    }
+}
+$done({body});
